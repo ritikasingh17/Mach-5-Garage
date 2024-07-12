13ad5883d73559b7a25691dcda9e62c4dc6cdcd9
Sujin <sujinbonnaud@gmail.com>
2024-07-11 20:51:43 -0500

    update db name

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; // Replace with your MySQL username
 $password = ""; // Replace with your MySQL password
-$dbname = "your_database_name"; // Replace with your database name
+$dbname = "cs4347"; // Replace with your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
